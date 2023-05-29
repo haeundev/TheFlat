4438fa73b6859bbfd555da89f4e4739e72968732
heoh <haeundev@gmail.com>
2023-05-29 18:07:09 +0900

    skybox, soundsource

---
+++ TheFlat/Assets/SoundSource.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SoundType
+{
+    Radio,
+    Telephone,
+    
+}
+
+public class SoundSource : MonoBehaviour
+{
+    private static readonly List<SoundSource> Instances = new();
+    [SerializeField] private int id;
+    [SerializeField] private SoundType type;
+    
+    
+    private void OnEnable()
+    {
+        Instances.Add(this);
+    }
+    
+    private void OnDisable()
+    {
+        Instances.Remove(this);
+    }
+}
